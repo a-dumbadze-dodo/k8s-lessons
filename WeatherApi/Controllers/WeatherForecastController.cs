ca96f37411d6f38d880464431f4c87bc41e0be26
a.dumbadze <a.dumbadze@dodopizza.com>
2020-10-09 15:17:46 +0300

    initial commit

---
+++ WeatherApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WeatherApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+        private static readonly string StaticId = Guid.NewGuid().ToString();
+        private static readonly string CreationDateTime = DateTime.Now.ToString();
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public WeatherForecast Get()
+        {
+            var rng = new Random();
+            return new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)],
+                StaticId = StaticId,
+                CreationDateTime = CreationDateTime
+            };
+        }
+    }
+}
