41aa021233b0dbe19747778667ab45222cbf38c5
a-doom <a.dumbadze@dodopizza.com>
2020-11-13 13:14:27 +0300

    add namespace

--- lesson_2.ps1
+++ lesson_2.ps1
@@ -5,6 +5,9 @@ docker tag test-k8s-3 alexeydoom/test-k8s-3     # переименовал, по
 docker push alexeydoom/test-k8s-3               # и запушил. Можно посмотреть тут https://hub.docker.com/r/alexeydoom/test-k8s-3
 
 
+# Создаем неймспейс
+kubectl create namespace study-mlc
+
 # =============================================================
 # Создаем файл k8s\pod.yaml
 # Создаем и конектимся к девовскому куберу
@@ -15,34 +18,34 @@ kubectl config get-contexts                                     # Смотрим
 
 # =============================================================
 # Создаем поду
-kubectl get pods -n dev-mlc                                     # Проверяем, что поды нет
-kubectl create -f ./k8s/pod.yaml -n dev-mlc                     # Создаем поду
-kubectl get pods -n dev-mlc                                     # Теперь пода есть
+kubectl get pods -n study-mlc                                     # Проверяем, что поды нет
+kubectl create -f ./k8s/pod.yaml -n study-mlc                     # Создаем поду
+kubectl get pods -n study-mlc                                     # Теперь пода есть
 
-kubectl port-forward weather-pod 5004:80 -n dev-mlc             # Пробрасываем порт до пода
+kubectl port-forward weather-pod 5004:80 -n study-mlc             # Пробрасываем порт до пода
 curl http://localhost:5004/WeatherForecast                      # Проверяем
 
 
 # =============================================================
 # Создаем сервис
-kubectl get pods --show-labels -n dev-mlc                               # Смотрим на лэйблы подов, проверяем, что у нас в селекторе так же
-kubectl create -f ./k8s/service.yaml -n dev-mlc                         # Создаем сервис
-kubectl get services -n dev-mlc                                         # Проверяем, что все создалось
-kubectl port-forward svc/weather-service 5005:80 -n dev-mlc             # Пробрасываем порт до сервиса
+kubectl get pods --show-labels -n study-mlc                               # Смотрим на лэйблы подов, проверяем, что у нас в селекторе так же
+kubectl create -f ./k8s/service.yaml -n study-mlc                         # Создаем сервис
+kubectl get services -n study-mlc                                         # Проверяем, что все создалось
+kubectl port-forward svc/weather-service 5005:80 -n study-mlc             # Пробрасываем порт до сервиса
 curl http://localhost:5005/WeatherForecast                              # Проверяем
 
 
 # =============================================================
 # Создаем деплойметн
-kubectl create -f ./k8s/deployment.yaml -n dev-mlc                      # Создаем деплоймент
-kubectl get deployment -n dev-mlc
-kubectl get pods --show-labels -n dev-mlc                               # Обращаем внимание на странное название второго пода
-kubectl delete pod weather-pod -n dev-mlc                               # Удаляем под созданный руками
+kubectl create -f ./k8s/deployment.yaml -n study-mlc                      # Создаем деплоймент
+kubectl get deployment -n study-mlc
+kubectl get pods --show-labels -n study-mlc                               # Обращаем внимание на странное название второго пода
+kubectl delete pod weather-pod -n study-mlc                               # Удаляем под созданный руками
 
 
 # =============================================================
 # Все чистим
-kubectl delete svc weather-service -n dev-mlc
-kubectl delete deployment weather-deployment -n dev-mlc
-kubectl delete pod weather-pod -n dev-mlc
+kubectl delete svc weather-service -n study-mlc
+kubectl delete deployment weather-deployment -n study-mlc
+kubectl delete pod weather-pod -n study-mlc
 
